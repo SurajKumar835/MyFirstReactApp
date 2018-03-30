61febdd5cef65c2d029e357ea6e15e6d577fe67e
Surajkumar <surajkumar.kumar835@gmail.com>
2018-03-30 18:49:01 +0530

    Edit 4

--- src/App.js
+++ src/App.js
@@ -5,22 +5,27 @@ class App extends Component {
   constructor(){
     super();
     this.state={
-      projects:[
-      {
-        title:"Suraj",
-        category:'WebDesign1'
-      },
-      {
-        title: "Kumar",
-        category: 'WebDesign2'
-      },
-      {
-        title: "Social App",
-        category: 'WebDesign3'
-      }
-      ]
+      projects:[]
     }
   }
+
+componentWillMount(){//lifecycle method of React
+this.setState({projects:[
+  {
+    title: "Suraj",
+    category: 'WebDesign1'
+  },
+  {
+    title: "Kumar",
+    category: 'WebDesign2'
+  },
+  {
+    title: "Social App",
+    category: 'WebDesign3'
+  }
+]});
+}
+
   render() {
     return (
       <div className="App">
