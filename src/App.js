e905fd98fb8b2dd0d2afb711c3c93ffcbf97486d
Surajkumar <surajkumar.kumar835@gmail.com>
2018-03-30 18:43:38 +0530

    Edit 3

--- src/App.js
+++ src/App.js
@@ -2,11 +2,30 @@ import React, { Component } from 'react';
 import Projects from './Components/Projects';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      projects:[
+      {
+        title:"Suraj",
+        category:'WebDesign1'
+      },
+      {
+        title: "Kumar",
+        category: 'WebDesign2'
+      },
+      {
+        title: "Social App",
+        category: 'WebDesign3'
+      }
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
       My FirstApp
-      <Projects/>
+      <Projects projects={this.state.projects}/>
       </div>
     );
   }
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
-
+import ProjectItem from "./ProjectItem";
 class Projects extends Component {
   render() {
-    return <div className="Projects">
-    My FirstApp</div>;
+    let projectItems;
+    if (this.props.projects) {
+      projectItems = this.props.projects.map(projects => {
+        // console.log(projects);
+        return <ProjectItem key={projects.title} project={projects} />;
+      });
+    }
+
+    return <div className="Projects">{projectItems}</div>;
   }
 }
 
