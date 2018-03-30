0fca04131505890bb1b0878393b9d4f31c441e57
Surajkumar <surajkumar.kumar835@gmail.com>
2018-03-30 18:05:32 +0530

    Edit 1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Projects from './Components/Projects';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      My FirstApp
+      <Projects/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/Projects.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Projects extends Component {
+  render() {
+    return <div className="Projects">
+    My FirstApp</div>;
+  }
+}
+
+export default Projects;
